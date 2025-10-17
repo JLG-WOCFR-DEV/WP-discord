feee307227e11f8097106355572cfedf38b2b1c2
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-17 12:15:03 +0200

    Normalize profile key sanitization separators

--- discord-bot-jlg/tests/phpunit/Test_Discord_Bot_JLG_Helpers.php
+++ discord-bot-jlg/tests/phpunit/Test_Discord_Bot_JLG_Helpers.php
@@ -275,6 +275,7 @@ class Test_Discord_Bot_JLG_Helpers extends TestCase {
             'underscore retained' => array('Already_Slug', 'already_slug'),
             'combined separators' => array('Profile_With Mixed-Separators', 'profile_with-mixed-separators'),
             'punctuation squashed into underscore' => array('Custom Key!!', 'custom_key'),
+            'mixed whitespace collapsed' => array("Tabs\t and\n new lines", 'tabs_and_new_lines'),
         );
     }
 
